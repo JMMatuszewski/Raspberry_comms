c8822861cc53aba1b545eb482f73e9cc293be652
JMMatuszewski <jmm.matuszewski.programista@gmail.com>
2024-09-05 19:21:22 +0200

    ethernet-test-done

--- Server/server_main.py
+++ Server/server_main.py
@@ -3,7 +3,8 @@ from datetime import datetime
 import threading
 from tcp_thread import tcp_thread
 from udp_thread import udp_thread
-
+# raspberry wi-fi: 192.168.100.21
+# raspberry eth0 : 10.10.0.11
 
 def main():
 
