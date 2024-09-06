240c1e6c30ea84bc1b87d7526ac1b2c106b9e64e
JMMatuszewski <jmm.matuszewski.programista@gmail.com>
2024-09-06 14:20:50 +0200

    ip-change for test

--- Client/main_thread.py
+++ Client/main_thread.py
@@ -2,6 +2,8 @@ from tcp_socket import TCPSocket
 from udp_socket import UDPSocket
 import socket
 from protocol_lib import PROTOCOL
+import msvcrt
+import time
 
 def main_thread():
 
@@ -10,14 +12,18 @@ def main_thread():
         print("Currently avalable protocols:")
         for key in PROTOCOL:
                 print(f'- {key}')
-        protocol = input('Please input a protocol You want to use:\n')
+        # protocol = input('Please input a protocol You want to use:\n')
+        protocol = "udp"
         protocol = protocol.upper()
         if protocol == "Q":
                 break
         elif protocol not in PROTOCOL:
                 print(f'protocol "{protocol}" is not available\n')
                 continue
-        msg = input("Please write a short message:\n")
+        # msg = input("Please write a short message:\n")
+        print("Make a move: ")
+        char = str(msvcrt.getch().decode())
+        print(char)
         
 
         # Basic way for chosing method of communication - for rework later.
@@ -26,7 +32,7 @@ def main_thread():
             try:
                 TCP = TCPSocket(PROTOCOL[protocol]['ip'],PROTOCOL[protocol]['port'])
                 TCP.create_socket()
-                TCP.send_msg(msg)
+                TCP.send_msg(char) #msg
                 print(TCP.rcv_msg())
             finally:
                 TCP.close_socket()
@@ -34,10 +40,11 @@ def main_thread():
             try:
                 UDP = UDPSocket(PROTOCOL[protocol]['ip'],PROTOCOL[protocol]['port'])
                 UDP.create_socket()
-                UDP.send_msg(msg)
+                UDP.send_msg(char)
+                #time.sleep(1)
                 print(UDP.rcv_msg())
             finally:
                 UDP.close_socket()
                 
-
-        input("Press button for another interation")
+        # TESTING STOPPER
+        # input("Press button for another interation")
--- Client/protocol_lib.py
+++ Client/protocol_lib.py
@@ -1,5 +1,8 @@
-PROTOCOL = { 'TCP': {'ip':'10.10.0.11','port':9999},
-             'UDP': {'ip':'10.10.0.11','port':9998}
+# PROTOCOL = { 'TCP': {'ip':'10.10.0.11','port':9999},
+#              'UDP': {'ip':'10.10.0.11','port':9998}
+#             }
+PROTOCOL = { 'TCP': {'ip':'127.0.0.1','port':9999},
+             'UDP': {'ip':'127.0.0.1','port':9998}
             }
 
 # SOCKET DICTIONARY WITH FACTORIES
